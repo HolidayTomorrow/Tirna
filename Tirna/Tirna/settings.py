8abca2f65be80169e772164bd24d94dbdfd12159
HolidayTomorrow <marcelo.rlopes@hotmail.com>
2025-02-24 11:02:04 -0300

    Adicionado de maneira dinâmica, os atributos da variável SECRET_KEY, que busca as informações do arquivo .env na pasta env_files.

--- Tirna/Tirna/settings.py
+++ Tirna/Tirna/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -21,7 +21,7 @@ DATA_DIR = BASE_DIR.parent / 'data' / 'web'
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-kl&3pqw6x#ei&@m+6(1jx!amri4ow2+&_9lm*(0=m&!9cd%ubb'
+SECRET_KEY = os.getenv('SECRET_KEY', 'change-me')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
