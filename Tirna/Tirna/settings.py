dae82267b4919e8071b81cb00215296bb31055ea
HolidayTomorrow <marcelo.rlopes@hotmail.com>
2025-02-24 11:19:22 -0300

    Adicionado os atributos corretos para as variáveis LANGUAGE_CODE, TIME_ZONE, STATIC_ROOT, MEDIA_URL e MEDIA_ROOT.

--- Tirna/Tirna/settings.py
+++ Tirna/Tirna/settings.py
@@ -110,9 +110,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-br'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Sao_Paulo'
 
 USE_I18N = True
 
@@ -123,6 +123,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = DATA_DIR / 'static'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = DATA_DIR / 'media' 
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
